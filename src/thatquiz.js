b0f2699c7678cdf876887166f607213f6b49259e
TheGamer X <thehackerdroid77@gmail.com>
2020-10-04 20:51:59 +0200

    Better configuration, sliders, better code layout

--- src/thatquiz.js
+++ src/thatquiz.js
@@ -3,25 +3,30 @@ h_data.running = false
 h_data.version = "1.8"
 
 function random(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min}
-function h_stop() {
-	console.info("Done")
-	h_clean()
-}
-
 function h_clean() {
 	h_data.running = false
-
 	menu = document.getElementById("h_injectmenu")
-	if (menu) {
-		menu.remove()
+	if (menu) menu.remove()
+}
+
+function h_request(url, nocache) {
+	var xmlHttp = new XMLHttpRequest()
+	url += (nocache) ? "?_=" + new Date().getTime() : "?v=" + Math.floor(h_data.version * 100)
+	xmlHttp.open("GET", url, false)
+	xmlHttp.send()
+	return xmlHttp.responseText
+}
+
+function h_updateprogress() {
+	if (!h_data.running) return
+	value = Date.now() - h_data.progresstart
+	document.getElementById("h_progress").style.width = Math.floor(value / h_data.waittime * 100) + "%"
+	if (!(value >= h_data.waittime)) {
+		setTimeout(h_updateprogress, 50)
 	}
 }
 
-function h_random() {
-	return random(+h_data.min, +h_data.max)
-}
-
-function h_toggle(e) {
+function h_stopbtn() {
 	if (h_data.running) {
 		h_data.wasrunning = true
 		h_data.running = false
@@ -31,50 +36,28 @@ function h_toggle(e) {
 		h_next()
 	}
 
-	e.innerHTML = h_data.running ? "Stop" : "Resume"
+	this.innerHTML = h_data.running ? "Stop" : "Resume"
 }
 
-function h_request(url, nocache) {
-	var xmlHttp = new XMLHttpRequest()
-	if (nocache) {
-		url = url + "?_=" + new Date().getTime()
-	} else {
-		url = url + "?v=" + Math.floor(h_data.version*100)
-	}
-	xmlHttp.open("GET", url, false)
-	xmlHttp.send()
-	return xmlHttp.responseText
-}
-
-function h_updateprogress() {
-	if (!h_data.running) {
-		return
-	}
-	value = Date.now() - h_data.progresstart
-	document.getElementById("h_progress").style.width = Math.floor(value / h_data.waittime * 100) + "%"
-	if (!(value >= h_data.waittime)) {
-		setTimeout(h_updateprogress, 50)
-	}
+function h_inputboxie(e, divide) {
+	value = (divide) ? e.value / 1000 : e.value
+	document.getElementById(e.id + "_num").value = value
 }
 
 function h_next() {
-	if (!h_data.running) {
-		return
-	}
-
+	if (!h_data.running) return
 	iswrong = false
-	total = parseInt(h_data.right.value, 10) + parseInt(h_data.wrong.value, 10)
 	wrong = h_data.wrong.value
 
 	input = document.querySelector("input#C")
-	if (input == null) {
-		h_stop()
+	if (!input) {
+		h_clean()
 		return
 	}
 
-	hackit = document.getElementsByClassName("qq nw")[0].innerHTML;
+	// Convert symbols to math
+	hackit = document.querySelector("table.g6cd tbody tr td.qq.nw").innerHTML;
 	for (let step = 0; step < 3; step++) {
-		hackit = hackit.replace(/\s/g, '') //Spaces
 		hackit = hackit.replace('×', "*") //Multiply
 		hackit = hackit.replace('÷', "/") //Divison
 		hackit = hackit.replace('–', "-") //Substraction
@@ -97,48 +80,43 @@ function h_next() {
 	// We did something wrong
 	if (h_data.wrong.value > wrong && !iswrong) {
 		console.info("I did ", hackit, " wrong!!")
-		h_stop()
+		h_clean()
+		return
 	}
 
-	if (!h_data.isbrute) {
-		randoms = h_random()
+	if (h_data.isbrute) {
+		randoms = 0
+	} else {
+		randoms = random(+h_data.delay - +h_data.variation, +h_data.delay + +h_data.variation)
 		if (randoms !== 0) {
 			h_data.waittime = randoms
 			h_data.progresstart = new Date()
 			document.getElementById("h_progress").style.width = 0
 			setTimeout(h_updateprogress, 0)
 		}
-	} else {
-		randoms = 0
 	}
 
 	setTimeout(h_next,randoms)
 }
 
 function h_apply() {
-	console.info("Starting")
-	form = document.forms["h_form"]
 	h_data.running = true
+	form = document.forms["h_form"]
 
-	h_data.min = form["h_value_min"].value
-	h_data.max = form["h_value_max"].value
+	h_data.delay = form["h_delay"].value
+	h_data.variation = form["h_variation"].value
 	h_data.miss = form["h_misschance"].value
 
 	h_data.lenght = document.getElementById("TESTLENGTH").value
 	h_data.right = document.getElementById("q917")
 	h_data.wrong = document.getElementById("q91a")
-	h_data.isbrute = (h_data.max === 0)
-
-
-	if (h_data.lenght == 101) {
-		h_data.isinfinite = true
-	}
+	h_data.isbrute = (h_data.delay == 0) ? true : false
 
 	document.getElementById("h_menu").remove()
-
 	injectmenu = document.getElementById("h_injectmenu")
 	document.getElementById("bz1x").after(injectmenu)
 	injectmenu.hidden = false
+	document.getElementById("h_stopbtn").addEventListener("click", h_stopbtn)
 	h_next()
 }
 
@@ -148,13 +126,13 @@ function h_init() {
 	islocal = (typeof h_islocal == "undefined")
 
 	if (islocal) {
-		console.info("Local")
-		html = h_request("http://127.0.0.1:5500/src/html.html")
-		css = h_request("http://127.0.0.1:5500/src/css.css")
+		console.info("Running in local mode. if you dont know what this is you did something wrong!")
+		path = "http://127.0.0.1:5500/src/"
 	} else {
-		html = h_request("https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/src/html.html")
-		css = h_request("https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/src/css.css")
+		path = "https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/src/"
 	}
+	html = h_request(path + "html.html")
+	css = h_request(path + "css.css")
 
 	style = document.createElement("style")
 	style.innerHTML = css
