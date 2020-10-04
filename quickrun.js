2d1328f7984256ae84855d4b2549e3313420588d
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2020-10-04 02:31:51 +0200

    quick run

--- quickrun.js
+++ quickrun.js
@@ -1,18 +1,6 @@
-# Quick Run
-
-```javascript
 xmlHttp = new XMLHttpRequest()
 xmlHttp.open("GET", "https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/dist/thatquiz.js?_=" + new Date().getTime(), false)
 xmlHttp.send()
 script = document.createElement("script")
 script.innerHTML = xmlHttp.responseText
 document.body.append(script)
-```
-
-# Instructions
-
-1. Open debug console (`CTRL + SHIFT + K`)
-2. Paste thatquiz.js contents
-3. Press (`ENTER`)
-4. Modify the configuration to ur liking
-5. Press `OK` button
