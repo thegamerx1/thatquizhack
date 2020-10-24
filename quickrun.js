6008414a8d96e17813bb3cfc014bca847ce6c939
TheGamer X <thehackerdroid77@gmail.com>
2020-10-24 11:26:41 +0200

    make script local variable

--- quickrun.js
+++ quickrun.js
@@ -2,7 +2,7 @@ function h_inject() {
 	var xmlHttp = new XMLHttpRequest()
 	xmlHttp.open("GET", "https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/dist/thatquiz.js?_=" + new Date().getTime(), false)
 	xmlHttp.send()
-	script = document.createElement("script")
+	var script = document.createElement("script")
 	script.innerHTML = xmlHttp.responseText
 	document.head.append(script)
 }
