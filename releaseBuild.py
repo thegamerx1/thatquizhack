6f19415be0e477c706101cca3dce092cc6d84601
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2020-10-25 18:22:07 +0100

    clean

--- releaseBuild.py
+++ releaseBuild.py
@@ -1,39 +1,38 @@
 import json
 import requests
-import os
 import time
 
 def readfile(file):
 	with open(file, "r") as f:
 		return f.read()
 
-class stage:
-	def __init__(self, name):
-		print(name, end="")
-		self.start = time.perf_counter()
+def setstage(name):
+	try:
+		setstage.oldtime
+	except AttributeError:
+		setstage.oldtime = ""
 
-	def done(self):
-		elapsed = str(round(time.perf_counter() - self.start, 2))
+	if (setstage.oldtime):
+		elapsed = str(round(time.perf_counter() - setstage.oldtime, 2))
 		print(" (" + elapsed + "ms)")
 
-stage0 = stage("Appending extensions..")
+	print(name, end="")
+	setstage.oldtime = time.perf_counter()
+
+
+setstage("Appending extensions..")
 javascript = readfile("src/thatquiz.js")
 extensions = json.loads(readfile("src/extensions.json"))
 javascript = javascript.replace("//InjectJSON//", "h_data.extensionlist = JSON.parse('" + json.dumps(extensions) + "')")
 for i in extensions["list"]:
 	javascript += "\n" + readfile("src/extensions/" + i + ".js")
-stage0.done()
 
 
-stage0 = stage("Compressing..")
+setstage("Compressing..")
 response = requests.post("https://htmlcompressor.com/compress", data={"code": javascript, "code_type": "js", "output_format": "text", "js_engine": "closure", "js_closure_single_line": 1})
 javascript = response.text
-stage0.done()
 
-stage0 = stage("Writting to file..")
+setstage("Done")
 with open("dist/thatquiz.js", "w") as f:
 	f.write(javascript)
 	f.close()
-stage0.done()
-
-os._exit(1)
