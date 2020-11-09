91e1950e7385ba77a9224b900066ac4c1c8f7e37
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2020-11-09 17:45:06 +0100

    forgot updating version

--- src/thatquiz.js
+++ src/thatquiz.js
@@ -1,6 +1,6 @@
 var h_data = {}
 //InjectJSON//
-h_data.version = "2.3"
+h_data.version = "2.5"
 
 function random(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min}
 function randomarray(array) {return array[Math.floor(Math.random() * array.length)];}
