8d09df04ee54a34cbbfab81611468bc506cc4f8f
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2020-11-11 20:05:07 +0100

    added fraction support

--- src/extensions/Exponents.js
+++ src/extensions/Exponents.js
@@ -3,17 +3,26 @@ function hex_Exponents(data, istest) {
 		return (typeof exponent === 'object' && exponent !== null)
 	}
 
-	// TODO: Fractions dont work
-
 	var cinput = data.table.querySelector("input#C")
-	cvalue = exponent.ca
+	var dinput = data.table.querySelector("input#D")
+
+	var stripped = stripHtml(exponent.ca)
+	if (dinput) {
+		let match = stripped.match(/(.*)\/(.*)/)
+		var cvalue = match[1]
+		var dvalue = match[2]
+	} else {
+		var cvalue = stripped
+	}
 
 	// * Miss chance
 	if (data.iswrong) {
 		cvalue -= random(-20, 20)
+		dvalue -= random(-20, 20)
 	}
 
 	cinput.value = cvalue
+	if (dinput) dinput.value = dvalue
 	exponent.v86()
 	return
 }
