55ff8a012076a5bbecc7891048c1f552922732c9
TheGamerX1 <thehackerdroid77@gmail.com>
2021-01-20 19:55:27 +0100

    add automatic solving

--- src/thatquiz.js
+++ src/thatquiz.js
@@ -7,36 +7,46 @@ hackclass = class {
 		//InjectExtensions//
 		this.islocal = (!this.extension.list)
 
-		var path
 		if (this.islocal) {
 			this.log("Running in LOCAL mode!")
-			path = "http://localhost:5500/src/"
+			this.path = "http://localhost:5500/"
 		} else {
-			path = "https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/src/"
+			this.path = "https://raw.githubusercontent.com/thegamerx1/thatquizhack/master/"
 		}
 
-		this.log("Loading extensions")
-		this.extension.list = (this.islocal) ? JSON.parse(this.request(path + "extensions.json")) : this.extension.list
+		if (typeof cp == "object") {
+			cp.test = function (a, d) {
+				var c = cp.g641(a)
+				var h = window.location.search.substr(1);
+				var b = parseInt(q9("fZk").value).toString(16);
+				var f = parseInt(a).toString(16),
+					c = (cp.fZ = "student?s=" + b + "&e=" + f + "&w=" + c + "&r=" + h),
+					c = jqh("/eu/" + c, null, cp.jqg);
+				return c
+			}
+		} else {
+			this.log("Loading extensions")
+			this.extension.list = (this.islocal) ? JSON.parse(this.request(this.path + "src/extensions.json")) : this.extension.list
 
-		//  ? Load extensions
-		for (i = 0; i < this.extension.list.list.length; i++) {
-			let name = this.extension.list.list[i]
-			this.log(name)
-			if (this.islocal) {
-				this.extension.func[name] = eval(this.request(path + "extensions/" + name + ".js") + name)
+			//  ? Load extensions
+			for (i = 0; i < this.extension.list.list.length; i++) {
+				let name = this.extension.list.list[i]
+				if (this.islocal) {
+					this.extension.func[name] = eval(this.request(this.path + "src/extensions/" + name + ".js") + name)
+				}
+
+				if (this.callextension(name, true).output) {
+					this.log("Test is: " + name)
+					this.test = name
+					break
+				}
 			}
 
-			if (this.callextension(name, true).output) {
-				this.log("Test is: " + name)
-				this.test = name
-				break
-			}
+			if (!this.test) return this.log("Could not find a extension for current test.")
 		}
 
-		if (!this.test) return this.log("Could not find a extension for current test.")
-
-		var html = (this.islocal) ? this.request(path + "html.html") : this.extension.html
-		var css = (this.islocal) ? this.request(path + "css.css") : this.extension.css
+		var html = (this.islocal) ? this.request(this.path + "src/html.html") : this.extension.html
+		var css = (this.islocal) ? this.request(this.path + "src/minify/css.css") : this.extension.css
 
 		var style = document.createElement("style")
 		style.innerHTML = css
@@ -47,24 +57,22 @@ hackclass = class {
 		document.head.append(style)
 
 		var form = document.forms["hackform"]
-		if (this.islocal) {
-			this.license(true)
-			form["delay"].value = 0
-			form["misschance"].value = 0
-		}
-
+		if (this.islocal) this.license(true)
 
 		// ? Update their status bcs im lazy to hardcode it
+		if (typeof cp == "object") {
+			let delay = form.querySelector("input[name=delay]")
+			delay.max = 15 * 60
+			delay.value = 60 * 7
+		}
 		form.querySelectorAll("input").forEach(element => {
 			element.dispatchEvent(new Event("input"))
 		})
 		this.hackinjectmenu = document.getElementById("hackinjectmenu")
 	}
 
-	log(text, ...args) {
-		// if (!this.islocal) return
-		args = (args.length > 0 ? args : 0)
-		console.log("%c[Hack]", 'background: #222; color: #ff7c55', text, args)
+	log(...text) {
+		console.log("%c[Hack]", 'background: #222; color: #ff7c55', ...text)
 	}
 	random(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min }
 	randomarray(array) { return array[Math.floor(Math.random() * array.length)]; }
@@ -72,23 +80,7 @@ hackclass = class {
 	isHidden(el) { return (el.style.display === "none")}
 	randomobject(obj) { var keys = Object.keys(obj); return obj[keys[Math.floor(Math.random() * keys.length)]] }
 	stripHtml(html) { let tmp = document.createElement("DIV"); tmp.innerHTML = html; return tmp.textContent || tmp.innerText || ""; }
-	// getPrimeNumbers(num) {
-	// 	var half = Math.floor(num / 2)
-	// 	var array = [1]
-	// 	var i, j
-	// 	num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2);
-	// 	for (i; i <= half; i += j) { num % i === 0 ? array.push(i) : false }
-	// 	array.push(num)
-	// 	return array
-	// }
-	// h_tomath(text) {
-	// 	var text = text.replaceAll("×", "*") //Multiply
-	// 	var text = text.replaceAll("÷", "/") //Divison
-	// 	var text = text.replaceAll("–", "-") //Substraction
-	// 	var text = text.replaceAll(",", ".") //Substraction
-	// 	var text = text.replaceAll("|", "")
-	// 	return text
-	// }
+
 	clean() {
 		this.running = false
 		document.getElementById("hackinjectmenu")
@@ -100,11 +92,17 @@ hackclass = class {
 		if (menu) menu.remove()
 		if (hmenu) hmenu.remove()
 		if (hstyle) hstyle.remove()
+		if (this.frame) this.frame.remove()
+		let doneorDoing = document.querySelectorAll(".doing, .done")
+		doneorDoing.forEach(e => {
+			e.classList.remove("done", "doing")
+		})
 	}
 
 	stop() {
 		this.log("Stopping")
 		this.clean()
+		window.dispatchEvent(new Event("hacked"))
 	}
 
 	request(url, nocache) {
@@ -112,7 +110,6 @@ hackclass = class {
 		url += (nocache) ? "?nocache=" + new Date().getTime() : "?v=" + this.version * 100
 		xhr.open("GET", url, false)
 		xhr.send()
-		// this.log("Response", [url, xhr.responseText])
 		return xhr.responseText
 	}
 
@@ -146,23 +143,30 @@ hackclass = class {
 	inputboxietime(e) {
 		var total = e.parentElement.querySelectorAll(".value")[1]
 		var each = e.parentElement.querySelectorAll(".value")[0]
-		var testlength = document.getElementById("TESTLENGTH").value
+		var testlength
+		if (typeof cp == "object") {
+			testlength = document.getElementById("etb").children.length
+		} else {
+			testlength = document.getElementById("TESTLENGTH").value
+		}
 
 		if (testlength != 101) {
 			var value = e.value * testlength
-			var minutes = Math.floor(value / 60)
-			var seconds = value % 60
-			total.innerHTML = "~" + minutes + "m " + seconds + "s"
+			let date = new Date(value * 1000).toISOString().substr(11, 5)
+			total.innerHTML = `~${date.substr(0, 2)}h ${date.substr(3)}m`
+			new Date(10 * 1000).toISOString()
 			total.removeAttribute("hide")
 		} else {
 			total.innerHTML = ""
 			total.setAttribute("hide", "true")
 		}
-		each.innerHTML = e.value + "s"
+		let date = new Date(e.value * 1000).toISOString().substr(14, 5)
+		each.innerHTML = `${date.substr(0, 2)}m ${date.substr(3)}s`
 	}
 
 	license(bypass) {
 		let licenseform = document.forms["hacklicense"]
+		if (!licenseform) return
 		if (!bypass && !licenseform["license"].checked) return
 		licenseform.remove()
 		document.forms["hackform"].style.display = ""
@@ -186,6 +190,7 @@ hackclass = class {
 		var oldwrong = this.getwrong()
 		var output = this.callextension(this.test)
 
+		if (document.getElementById("g902")) document.getElementById("g902").click()
 		// We are done
 		if (output.output === "done") return this.stop()
 
@@ -218,11 +223,43 @@ hackclass = class {
 		}
 	}
 
+	hackyNext(done) {
+		if (done) {
+			let temprow = document.getElementById("etb").querySelector(".doing")
+			temprow.classList.add("done")
+			temprow.classList.remove("doing")
+		}
+		let rows = document.getElementById("etb").querySelectorAll("tr:not(.done)")
+		if (rows.length == 0) return this.stop()
+		let row = rows[rows.length-1]
+		row.classList.add("doing")
+		let url = eval(row.querySelector(".g63x").href.replace("javascript:", ""))
+		let window = this.frame.contentWindow
+		window.top.frames.length = 0 // spoofy
+		this.frame.src = url
+	}
+
+	hackyInject() {
+		let window = this.frame.contentWindow
+		let script = document.createElement("script")
+		script.src = this.islocal ? this.path + "src/thatquiz.js" : this.path + "dist/thatquiz.js"
+		window.document.body.append(script)
+		script.onload = () => {
+			let form = window.document.forms["hackform"]
+			form["delay"].value = this.delay / 1000 / document.querySelector(".doing td.numeric.w8o").innerHTML
+			form["variation"].value = this.variation
+			form["misschance"].value = this.miss
+			window.hack.license(true)
+			window.hack.apply()
+		}
+		this.frame.contentWindow.addEventListener("hacked", this.hackyNext.bind(this, true))
+	}
+
 	apply() {
-		if (!this.gettable()) return this.log("No test found!")
+		if (typeof cp !== "object" && !this.gettable()) return this.log("No test found!")
 
 		this.running = true
-		var form = document.forms["hackform"]
+		const form = document.forms["hackform"]
 		this.delay = form["delay"].value * 1000
 		this.variation = form["variation"].value
 		this.miss = form["misschance"].value
@@ -231,6 +268,16 @@ hackclass = class {
 		this.istest = form["istest"].checked
 
 		document.getElementById("hackmenu").remove()
+		if (typeof cp == "object") {
+			this.frame = document.createElement("iframe")
+			this.frame.id = "hackyFrame"
+			this.frame.parent = null
+			document.querySelector("table#a3v.bZL tbody tr td.w8q.i9b").append(this.frame)
+			this.frame.addEventListener("load", this.hackyInject.bind(this))
+			this.hackyNext()
+			return
+		}
+
 		var injectmenu = document.getElementById("hackinjectmenu")
 		document.getElementById("bz1x").after(injectmenu)
 		if (this.istest) {
